d3ff4615403835277f63c23b3c1c836f85a6da61
DESKTOP-JM33RC1\no1s <katsuki.rintaro@no1s.biz>
2023-06-26 16:42:10 +0900

    finish

---
+++ src/main/webapp/Accodion/app.js
@@ -0,0 +1,46 @@
+function nextFrame(cd) {
+	window.requestAnimationFrame(function() {
+		window.requestAnimationFrame(cd)
+	})
+}
+
+const app = Vue.createApp({})
+
+app.component('my-sample-accordion', {
+	template: '#my-sample-accordion',
+	props: {
+		title: {
+			type: String,
+			required: true
+		}
+	},
+	data: function() {
+		return {
+			isShown: false
+		}
+	},
+	methods: {
+		enter: function(el, done) {
+			el.style.overflow = "hidden"
+			const height = el.scrollHeight
+			el.style.height = "0"
+			
+			nextFrame(function() {
+				el.style.height = height + "px"
+			})
+		},
+		leave: function(el) {
+			el.style.overflow = "hidden"
+			el.style.height = el.scrollHeight + "px"
+			
+			nextFrame(function() {
+				el.style.height = "0"
+			})
+		},
+		afterTransition: function(el) {
+			el.style.overflow = ""
+			el.style.height = ""
+		}
+	}
+})
+app.mount('#app')
